b56278dfdc31959e2e2fd62aad6217fdefcb1b0f
Tommy <thantzinmin.us@gmail.com>
2025-02-16 02:48:08 +0700

    Add Expense Screen Changing

--- app/src/main/java/com/example/pennytrack/ui/screens/AddExpenseScreen.kt
+++ app/src/main/java/com/example/pennytrack/ui/screens/AddExpenseScreen.kt
@@ -2,35 +2,34 @@ package com.example.pennytrack.ui.screens
 
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
-import androidx.compose.material3.TextFieldDefaults.shape
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.example.pennytrack.viewmodels.ExpenseViewModel
 import com.example.pennytrack.data.models.Expense
-import com.example.pennytrack.ui.theme.AddScreenBackground
-import com.example.pennytrack.ui.theme.TopAppBarColor
-import com.example.pennytrack.ui.theme.softPurple
+import com.example.pennytrack.ui.theme.md_theme_light_onPrimaryContainer
+import com.example.pennytrack.ui.theme.md_theme_light_primary
+import com.example.pennytrack.ui.theme.md_theme_light_primaryContainer
+import com.example.pennytrack.ui.theme.md_theme_light_surface
+import com.example.pennytrack.ui.theme.md_theme_light_surfaceVariant
 import java.util.*
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AddExpenseScreen(navController: NavController, expenseViewModel: ExpenseViewModel = viewModel()) {
+fun AddExpenseScreen(
+    navController: NavController,
+    expenseViewModel: ExpenseViewModel = viewModel()
+) {
     var expenseTitle by remember { mutableStateOf("") }
     var expenseAmount by remember { mutableStateOf("") }
     var expenseDescription by remember { mutableStateOf("") }
@@ -39,7 +38,6 @@ fun AddExpenseScreen(navController: NavController, expenseViewModel: ExpenseView
 
     val calendar = Calendar.getInstance()
 
-    // Date Picker
     val datePickerDialog = DatePickerDialog(
         navController.context,
         { _, year, month, dayOfMonth ->
@@ -50,7 +48,6 @@ fun AddExpenseScreen(navController: NavController, expenseViewModel: ExpenseView
         calendar.get(Calendar.DAY_OF_MONTH)
     )
 
-    // Time Picker
     val timePickerDialog = TimePickerDialog(
         navController.context,
         { _, hourOfDay, minute ->
@@ -61,134 +58,219 @@ fun AddExpenseScreen(navController: NavController, expenseViewModel: ExpenseView
         true
     )
 
-    Column(modifier = Modifier.fillMaxSize().background(color = Color.White).padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center) {
-        Text(text = "Add Expense", style = MaterialTheme.typography.titleLarge)
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        TextField(
-            modifier = Modifier.fillMaxWidth().padding(16.dp),
-            value = expenseTitle,
-            onValueChange = { expenseTitle = it },
-            label = { Text("Expense Title")},
-            shape = RoundedCornerShape(8.dp),
-            colors = TextFieldDefaults.outlinedTextFieldColors(
-                containerColor = Color(0xFFE0F7FA),
-                focusedBorderColor = Color(0xFF00796B),
-                unfocusedBorderColor = Color(0xFFB2DFDB)
-            )
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        TextField(
-            modifier = Modifier.fillMaxWidth().padding(16.dp),
-            value = expenseAmount,
-            onValueChange = { expenseAmount = it },
-            label = { Text("Amount")},
-            shape = RoundedCornerShape(8.dp),
-            colors = TextFieldDefaults.outlinedTextFieldColors(
-                containerColor = Color(0xFFE0F7FA),
-                focusedBorderColor = Color(0xFF00796B),
-                unfocusedBorderColor = Color(0xFFB2DFDB)
-            )
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        TextField(
-            modifier = Modifier.fillMaxWidth().padding(16.dp),
-            value = expenseDescription,
-            onValueChange = { expenseDescription = it },
-            label = { Text("Description") },
-            shape = RoundedCornerShape(8.dp),
-            colors = TextFieldDefaults.outlinedTextFieldColors(
-                containerColor = Color(0xFFE0F7FA),
-                focusedBorderColor = Color(0xFF00796B),
-                unfocusedBorderColor = Color(0xFFB2DFDB)
-            )
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        TextField(
-            modifier = Modifier.fillMaxWidth().padding(16.dp),
-            value = expenseDate,
-            onValueChange = {},
-            label = { Text("Expense Date") },
-            readOnly = true,
-            trailingIcon = {
-                IconButton(onClick = { datePickerDialog.show() }) {
-                    Icon(Icons.Default.Edit, contentDescription = "Pick Date")
-                }
-            },
-            shape = RoundedCornerShape(8.dp),
-            colors = TextFieldDefaults.outlinedTextFieldColors(
-                containerColor = Color(0xFFE0F7FA),
-                focusedBorderColor = Color(0xFF00796B),
-                unfocusedBorderColor = Color(0xFFB2DFDB)
-            )
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        TextField(
-            modifier = Modifier.fillMaxWidth().padding(16.dp),
-            value = expenseTime,
-            onValueChange = {},
-            label = { Text("Expense Time") },
-            readOnly = true,
-            trailingIcon = {
-                IconButton(onClick = { timePickerDialog.show() }) {
-                    Icon(Icons.Default.Edit, contentDescription = "Pick Time")
-                }
-            },
-            shape = RoundedCornerShape(8.dp),
-            colors = TextFieldDefaults.outlinedTextFieldColors(
-                containerColor = Color(0xFFE0F7FA),
-                focusedBorderColor = Color(0xFF00796B),
-                unfocusedBorderColor = Color(0xFFB2DFDB)
-            )
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Row(){
-
-            Button(onClick = { navController.popBackStack() },
-                modifier = Modifier.padding(32.dp),
-                colors = ButtonDefaults.buttonColors(containerColor = TopAppBarColor)
-                ){
-                Text("Cancel", fontWeight = FontWeight.Bold, color = Color.DarkGray)
-            }
-
-            Button(onClick = {
-                val newId = expenseViewModel.expenses.value.size + 1
-                val expense = Expense(
-                    id = newId,
-                    title = expenseTitle,
-                    amount = expenseAmount.toFloatOrNull() ?: 0f,
-                    description = expenseDescription,
-                    date = expenseDate,
-                    time = expenseTime
+    Scaffold(
+        topBar = {
+            SmallTopAppBar(
+                title = {
+                    Text(
+                        "Add Expense",
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Medium
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(Icons.Filled.ArrowBack, "Back")
+                    }
+                },
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = md_theme_light_surface,
+                    titleContentColor = md_theme_light_primary
                 )
-                // Save the new expense in the ViewModel
-                expenseViewModel.addExpense(expense)
-                navController.popBackStack()  // Navigate back after adding the expense
-            },
-                modifier = Modifier.padding(32.dp),
-                colors = ButtonDefaults.buttonColors(containerColor = TopAppBarColor)) {
-                Text("Add", fontWeight = FontWeight.Bold, color = Color.DarkGray)
+            )
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(horizontal = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text("Expense Title", modifier = Modifier.fillMaxWidth()
+                .padding(top = 2.dp),
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.Medium,
+                color = md_theme_light_onPrimaryContainer
+            )
+
+            // Amount Card
+            Card(
+                modifier = Modifier.fillMaxWidth(),
+                colors = CardDefaults.cardColors(
+                    containerColor = md_theme_light_surfaceVariant
+                ),
+                elevation = CardDefaults.cardElevation(0.dp)
+            ) {
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth()
+                        .padding(16.dp),
+                    value = expenseTitle,
+                    onValueChange = { expenseTitle = it },
+                    label = { Text("Title") },
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedBorderColor = md_theme_light_primary,
+                        unfocusedBorderColor = md_theme_light_primary.copy(alpha = 0.5f)
+                    ),
+                    singleLine = true
+                )
+
             }
 
+            Text("Amount & Description", modifier = Modifier.fillMaxWidth()
+                .padding(top = 2.dp),
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.Medium,
+                color = md_theme_light_onPrimaryContainer
+            )
+            // Details Card
+            Card(
+                modifier = Modifier.fillMaxWidth(),
+                colors = CardDefaults.cardColors(
+                    containerColor = md_theme_light_surfaceVariant
+                ),
+                elevation = CardDefaults.cardElevation(0.dp)
+            ) {
+                Column(
+                    modifier = Modifier.padding(16.dp),
+                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+
+                    OutlinedTextField(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        value = expenseAmount,
+                        onValueChange = { expenseAmount = it },
+                        label = { Text("Amount ($)") },
+                        colors = OutlinedTextFieldDefaults.colors(
+                            focusedBorderColor = md_theme_light_primary,
+                            unfocusedBorderColor = md_theme_light_primary.copy(alpha = 0.5f),
+                            //focusedLabelColor = MaterialTheme.colorScheme.primary,
+                            //unfocusedLabelColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.5f)
+                        ),
+                        singleLine = true
+                    )
+
+                    OutlinedTextField(
+                        modifier = Modifier.fillMaxWidth(),
+                        value = expenseDescription,
+                        onValueChange = { expenseDescription = it },
+                        label = { Text("Description") },
+                        colors = OutlinedTextFieldDefaults.colors(
+                            focusedBorderColor = md_theme_light_primary,
+                            unfocusedBorderColor = md_theme_light_primary.copy(alpha = 0.5f)
+                        ),
+                        minLines = 2
+                    )
+                }
+            }
+
+            Text("Date & Time", modifier = Modifier.fillMaxWidth()
+                .padding(top = 2.dp),
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.Medium,
+                color = md_theme_light_onPrimaryContainer
+            )
+            // Date and Time Card
+            Card(
+                modifier = Modifier.fillMaxWidth(),
+                colors = CardDefaults.cardColors(
+                    containerColor = md_theme_light_surfaceVariant
+                ),
+                elevation = CardDefaults.cardElevation(0.dp)
+            ) {
+                Column(
+                    modifier = Modifier.padding(16.dp),
+                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    OutlinedTextField(
+                        modifier = Modifier.fillMaxWidth(),
+                        value = expenseDate,
+                        onValueChange = {},
+                        label = { Text("Date") },
+                        readOnly = true,
+                        trailingIcon = {
+                            IconButton(onClick = { datePickerDialog.show() }) {
+                                Icon(
+                                    Icons.Default.Edit,
+                                    contentDescription = "Pick Date",
+                                    tint = md_theme_light_primary
+                                )
+                            }
+                        },
+                        colors = OutlinedTextFieldDefaults.colors(
+                            focusedBorderColor = md_theme_light_primary,
+                            unfocusedBorderColor = md_theme_light_primary.copy(alpha = 0.5f)
+                        )
+                    )
+
+                    OutlinedTextField(
+                        modifier = Modifier.fillMaxWidth(),
+                        value = expenseTime,
+                        onValueChange = {},
+                        label = { Text("Time") },
+                        readOnly = true,
+                        trailingIcon = {
+                            IconButton(onClick = { timePickerDialog.show() }) {
+                                Icon(
+                                    Icons.Default.Edit,
+                                    contentDescription = "Pick Time",
+                                    tint = md_theme_light_primary
+                                )
+                            }
+                        },
+                        colors = OutlinedTextFieldDefaults.colors(
+                            focusedBorderColor = md_theme_light_primary,
+                            unfocusedBorderColor = md_theme_light_primary.copy(alpha = 0.5f)
+                        )
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            // Bottom Buttons
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                OutlinedButton(
+                    onClick = { navController.popBackStack() },
+                    modifier = Modifier.weight(1f),
+                    colors = ButtonDefaults.outlinedButtonColors(
+                        contentColor = md_theme_light_primary
+                    )
+                ) {
+                    Text("Cancel")
+                }
+
+                Button(
+                    onClick = {
+                        val newId = expenseViewModel.expenses.value.size + 1
+                        val expense = Expense(
+                            id = newId,
+                            title = expenseTitle,
+                            amount = expenseAmount.toFloatOrNull() ?: 0f,
+                            description = expenseDescription,
+                            date = expenseDate,
+                            time = expenseTime
+                        )
+                        expenseViewModel.addExpense(expense)
+                        navController.popBackStack()
+                    },
+                    modifier = Modifier.weight(1f),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = md_theme_light_primary
+                    )
+                ) {
+                    Text("Add Expense")
+                }
+            }
         }
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun AddExpenseScreenPreview() {
-    AddExpenseScreen(navController = rememberNavController())
 }
